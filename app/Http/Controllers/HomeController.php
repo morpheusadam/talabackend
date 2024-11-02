af84e1a67e521a605c9237f73ec020a36f69ab8e
morpheusadam <morpheusadam95@gmail.com>
2024-11-02 12:15:21 +0000

    refactor: update controllers and move Livewire components

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
 {
     //
+
     public function index()
     {
         return view('index');
     }
-
 }
